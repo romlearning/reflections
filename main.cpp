2ba798c8550d5e3b846b198bf2ec3af47e465f3e
romlearning <rkk.enlightened@gmail.com>
2015-04-25 23:35:19 +0300

    Using func2() from func2.hpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
+#include "func2.hpp"
+
 using std::cout;
 using std::endl;
 
 int main() {
 	cout << "Hello git!" << endl;
 	cout << "This is a modification" << endl;
+	
+	func2();
 
 	return 0;
 }
